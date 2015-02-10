e2b1373b75a0e4f9c7f0fa455c1e7453087e3352
Bei Zhang <bzhang@shapesecurity.com>
2015-02-10 00:51:21 -0800

    fixes #40

--- test/failure.js
+++ test/failure.js
@@ -355,5 +355,8 @@ suite("Parser", function () {
     testParseFailure("#=", "Unexpected token ILLEGAL");
     testParseFailure("**", "Unexpected token *");
     testParseFailure("({a = 0})", "Unexpected token )");
+    //testParseFailure("({a(b,c){let b;}})", "Duplicate parameter binding in method definition");
+    //testParseFailure("({a(b,c){let d; let b;}})", "Duplicate parameter binding in method definition");
+    //testParseFailure("({a(b,c){let d,e,b;}})", "Duplicate parameter binding in method definition");
   });
 });

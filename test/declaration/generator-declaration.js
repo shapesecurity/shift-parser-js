cf5a33328e684627fece6db2e77ca69b5f3639d3
Michael Ficarra <git@michael.ficarra.me>
2015-08-05 08:29:57 -0700

    additional test for yield in generator

--- test/declaration/generator-declaration.js
+++ test/declaration/generator-declaration.js
@@ -177,6 +177,7 @@ suite("Parser", function () {
 
     testParseFailure("label: function* a(){}", "Unexpected token \"*\"");
     testParseFailure("function*g(){ var yield; }", "Unexpected token \"yield\"");
+    testParseFailure("function*g(){ var yield = 1; }", "Unexpected token \"yield\"");
     testParseFailure("function*g(){ function yield(){}; }", "Unexpected token \"yield\"");
     testParseFailure("function*g(){ (function yield(){}); }", "Unexpected token \"yield\"");
 

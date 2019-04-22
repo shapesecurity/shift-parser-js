d321f8d6c0d7c1e1c59a98914817fb8ea99bdee6
mbruce <max.bruce12@gmail.com>
2019-04-22 16:56:35 -0700

    add-tests

--- test/miscellaneous/async-await.js
+++ test/miscellaneous/async-await.js
@@ -198,6 +198,74 @@ suite('async', () => {
         body: { type: 'FunctionBody', directives: [], statements: [] },
       }
     );
+
+    testParseFailure('let b = async () => []; for (a in await b());', 'Unexpected identifier');
+    testParse('async () => { let b = async () => []; for (a in await b()); }', expr,
+    {
+      body: {
+        directives: [],
+        statements: [
+          {
+            declaration: {
+              declarators: [
+                {
+                  binding: {
+                    name: 'b',
+                    type: 'BindingIdentifier',
+                  },
+                  init: {
+                    body: {
+                      elements: [],
+                      type: 'ArrayExpression',
+                    },
+                    isAsync: true,
+                    params: {
+                      items: [],
+                      rest: null,
+                      type: 'FormalParameters',
+                    },
+                    type: 'ArrowExpression',
+                  },
+                  type: 'VariableDeclarator',
+                },
+              ],
+              kind: 'let',
+              type: 'VariableDeclaration',
+            },
+            type: 'VariableDeclarationStatement',
+          },
+          {
+            body: {
+              type: 'EmptyStatement',
+            },
+            left: {
+              name: 'a',
+              type: 'AssignmentTargetIdentifier',
+            },
+            right: {
+              expression: {
+                arguments: [],
+                callee: {
+                  name: 'b',
+                  type: 'IdentifierExpression',
+                },
+                type: 'CallExpression',
+              },
+              type: 'AwaitExpression',
+            },
+            type: 'ForInStatement',
+          },
+        ],
+        type: 'FunctionBody',
+      },
+      isAsync: true,
+      params: {
+        items: [],
+        rest: null,
+        type: 'FormalParameters',
+      },
+      type: 'ArrowExpression',
+    });
   });
 
   suite('functions', () => {

6218534ad8efe92d517f1e58d62314fb3be139d1
mbruce <max.bruce12@gmail.com>
2019-04-22 17:06:12 -0700

    add tests, lint

---
+++ test/statements/for-await-statement.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2019 Shape Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+let stmt = require('../helpers').stmt;
+let testParse = require('../assertions').testParse;
+let testParseFailure = require('../assertions').testParseFailure;
+
+suite('Parser', () => {
+  suite('for await statement', () => {
+
+
+    testParse('for await([{a=0}] of b);', stmt, {
+      type: 'ForAwaitStatement',
+      left: {
+        type: 'ArrayAssignmentTarget',
+        rest: null,
+        elements: [{
+          type: 'ObjectAssignmentTarget',
+          properties: [{
+            type: 'AssignmentTargetPropertyIdentifier',
+            binding: { type: 'AssignmentTargetIdentifier', name: 'a' },
+            init: { type: 'LiteralNumericExpression', value: 0 } }],
+          rest: null,
+        }] },
+      right: { type: 'IdentifierExpression', name: 'b' },
+      body: { type: 'EmptyStatement' },
+    });
+
+    testParseFailure('for await(let of 0);', 'Unexpected number');
+    testParseFailure('for await(this of 0);', 'Invalid left-hand side in for-await');
+
+    testParseFailure('for await(var a = 0 of b);', 'Invalid variable declaration in for-await statement');
+    testParseFailure('for await(let a = 0 of b);', 'Invalid variable declaration in for-await statement');
+    testParseFailure('for await(const a = 0 of b);', 'Invalid variable declaration in for-await statement');
+
+    testParseFailure('for await(({a}) of 0);', 'Invalid left-hand side in for-await');
+    testParseFailure('for await(([a]) of 0);', 'Invalid left-hand side in for-await');
+
+    testParseFailure('for await(var a of b, c);', 'Unexpected token ","');
+    testParseFailure('for await(a of b, c);', 'Unexpected token ","');
+    testParseFailure('for await(let a in b);', 'Unexpected token "in"');
+    testParseFailure('for await(a in b);', 'Unexpected token "in"');
+    testParseFailure('for await(;;);', 'Unexpected token ";"');
+    testParseFailure('for await(let a;;;);', 'Unexpected token ";"');
+    testParseFailure('let b = []; for await(a in b);', 'Unexpected token "in"');
+    testParseFailure('for await(;;);', 'Unexpected token ";"');
+    testParseFailure('let b = []; for await(a in b);', 'Unexpected token "in"');
+  });
+});
